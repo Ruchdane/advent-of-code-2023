c384fe351c9db2408745b54bd73ef5cace548c8f
Ruchdane AMADOU <ruchdaneabiodun@gmail.com>
2023-12-07 05:23:46 +0100

    feat : Advent of code day 2

--- src/bin/day_2/main.rs
+++ src/bin/day_2/main.rs
@@ -1,5 +1,12 @@
+use std::fs;
+
 fn main() {
-    println!("Hello, World");
+    let games = fs::read_to_string("data/conundrum.txt").unwrap();
+    let bag = Bag::new(14, 13, 12);
+    let possible_games = games_possible_for_bag(games.as_str(), bag);
+    let connudrum: u32 = possible_games.iter().sum();
+    let power = games_power(games.as_str());
+    println!("Conundrum {:?} to the power of {}", connudrum, power);
 }
 #[derive(Debug, PartialEq)]
 struct Bag {
@@ -25,9 +32,8 @@ fn bag_from_reveal(reveal: &str) -> Bag {
     if reveal.is_empty() {
         return bag;
     }
-    reveal.split(',').into_iter().for_each(|cube_count| {
+    reveal.split(',').for_each(|cube_count| {
         let split: Vec<&str> = cube_count.trim_start().split(' ').collect();
-        println!("{:?}", split);
         let count: u32 = split[0].parse().unwrap();
         let color = split[1];
         match color {
@@ -44,7 +50,7 @@ fn minimum_bag_for_game(game: &str) -> Bag {
     if game.is_empty() {
         return bag;
     }
-    game.split(';').into_iter().for_each(|reveal| {
+    game.split(';').for_each(|reveal| {
         let reveal_bag = bag_from_reveal(reveal);
         bag.blue = std::cmp::max(bag.blue, reveal_bag.blue);
         bag.red = std::cmp::max(bag.red, reveal_bag.red);
@@ -53,11 +59,50 @@ fn minimum_bag_for_game(game: &str) -> Bag {
     bag
 }
 
-fn games_possible_for_bag(games: &str, bag: Bag) {
-    todo!()
+fn games_power(games: &str) -> u32 {
+    if games.is_empty() {
+        return 0;
+    }
+    games
+        .split('\n')
+        .map(parse_game)
+        .map(|(_games, bag)| bag.green * bag.red * bag.blue)
+        .sum()
+}
+fn games_possible_for_bag(games: &str, bag: Bag) -> Vec<u32> {
+    if games.is_empty() {
+        return vec![];
+    }
+    games
+        .split('\n')
+        .map(parse_game)
+        .filter_map(|(game_number, minimum_bag)| {
+            if minimum_bag.blue <= bag.blue
+                && minimum_bag.green <= bag.green
+                && minimum_bag.red <= bag.red
+            {
+                return Some(game_number);
+            }
+            None
+        })
+        .collect()
+}
+
+fn parse_game(game: &str) -> (u32, Bag) {
+    if game.len() < 5 {
+        panic!("Could not parse games")
+    }
+    let split: Vec<&str> = game[5..].split(':').collect();
+    if split.len() == 2 {
+        let game_number: u32 = split[0].parse().unwrap();
+        let bag = minimum_bag_for_game(split[1]);
+        return (game_number, bag);
+    }
+    panic!("Could not parse games")
 }
 
 mod test {
+    #![allow(unused)]
     use crate::*;
     fn assert_reveal_conversion(reveal: &str, expected: Bag) {
         let result = bag_from_reveal(reveal);
@@ -107,5 +152,28 @@ mod test {
         let game = "1 green, 3 red, 6 blue; 3 green, 6 red; 3 green, 15 blue, 14 red";
         let expected = Bag::new(15, 3, 14);
         assert_minimum_bag_for_game(game, expected);
+    }
+
+    fn assert_games_possible_for_bag(games: &str, bag: Bag, expected: Vec<u32>) {
+        let result = games_possible_for_bag(games, bag);
+        assert_eq!(result, expected)
+    }
+
+    #[test]
+    fn can_find_possible_games_for_empty_set() {
+        let games = "";
+        let bag = Bag::empty();
+        let expected = vec![];
+        assert_games_possible_for_bag(games, bag, expected);
+    }
+
+    #[test]
+    fn can_find_possible_games_for_non_empty_set() {
+        let games = "Game 1: 3 green, 7 blue; 7 red, 4 blue; 5 blue, 6 red, 2 green
+Game 2: 9 green; 8 green, 4 blue; 6 blue, 2 red, 1 green; 4 green, 1 blue; 5 blue, 2 green, 2 red
+Game 3: 3 red, 1 green, 5 blue; 1 red; 3 blue, 4 red; 3 blue, 1 green, 5 red";
+        let bag = Bag::new(8, 4, 8);
+        let expected = vec![1, 3];
+        assert_games_possible_for_bag(games, bag, expected);
     }
 }
