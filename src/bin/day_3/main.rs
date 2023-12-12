63b529021ef51abef5ab109addbd5cf2742e8e4c
Ruchdane AMADOU <ruchdaneabiodun@gmail.com>
2023-12-12 12:49:12 +0100

    🧪 : Add test case for Domain from number

--- src/bin/day_3/main.rs
+++ src/bin/day_3/main.rs
@@ -6,6 +6,7 @@ fn main() {
 /// ****************************
 /// *[the length of the number]*
 /// ****************************
+#[derive(PartialEq, Debug)]
 struct Domain {
     origin: usize,
     length: usize,
@@ -23,10 +24,14 @@ impl PuzzlePart {
     }
 }
 impl Domain {
+    fn new(origin: usize, length: usize) -> Self {
+        Self { origin, length }
+    }
+
     fn is_in_Domain(puzzle_part_origin: usize) -> bool {
         unimplemented!()
     }
-    fn domain(part: PuzzlePart) -> Self {
+    fn from_number(origin: usize, value: u32) -> Self {
         unimplemented!()
     }
 }
@@ -37,4 +42,26 @@ fn filter_adjacent_number(
     next_line: &PuzzleLine,
 ) -> Vec<u32> {
     unimplemented!()
+}
+
+#[cfg(test)]
+mod test {
+    use crate::Domain;
+
+    #[test]
+    fn can_convert_number_at_the_begining() {
+        let origin = 0;
+        let value = 120;
+        let expected = Domain::new(0, 122);
+        let result = Domain::from_number(origin, value);
+        assert_eq!(result, expected)
+    }
+    #[test]
+    fn can_convert_number_in_the_middle() {
+        let origin = 10;
+        let value = 120;
+        let expected = Domain::new(9, 122);
+        let result = Domain::from_number(origin, value);
+        assert_eq!(result, expected)
+    }
 }
